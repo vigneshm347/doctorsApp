1e70c307de6d08525b2e94fac7c22c06dddb8311
Vignesh Manivasagam <vmanivasagam@nexient.com>
2019-02-13 14:28:25 -0500

    minor fixes

--- src/app/g-api.service.ts
+++ src/app/g-api.service.ts
@@ -9,7 +9,7 @@ export class GApiService {
   constructor(private http: Http) { }
 
   getLocation(origin, destination){
-    this.url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin},MA&destinations=${destination}&departure_time=now&key= AIzaSyDAmA_VZYhPC8flnn1h5SFyCPI8_EObPw4`;
+    this.url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin},MA&destinations=${destination}&departure_time=now&key={apikey}`;
     return this.http.get(this.url);
 
   }
